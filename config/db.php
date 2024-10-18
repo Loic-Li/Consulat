958179d240fad72b0f92ec3b5d1ee92a66824511
Loic <loic.li001@gmail.com>
2024-10-18 12:21:18 +0200

    bd connexion

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "localhost"; // ou l'adresse de ton serveur
+$username = "root"; // ton nom d'utilisateur MySQL
+$password = ""; // ton mot de passe MySQL
+$dbname = "consulat"; // le nom de ta base de données
+
+// Créer la connexion
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+?>

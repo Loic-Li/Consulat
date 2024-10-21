13b6ef98cd41ad26540d2ccaf30a6457df733ac2
Loic <loic@PCdeLoic.home>
2024-10-21 15:52:34 +0200

    changement du type pour inscription et connexion

--- includes/header.php
+++ includes/header.php
@@ -1,14 +1,11 @@
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Consulat de Chine</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-
     <?php
         // Déterminer quel CSS charger
         $basePath = (strpos($_SERVER['SCRIPT_NAME'], '/views/') !== false) ? '../' : '';
@@ -17,6 +14,7 @@
             echo '<link rel="stylesheet" href="' . $basePath . 'assets/css/' . htmlspecialchars($cssFile) . '.css">';
         }
     ?>
+    <link rel="stylesheet" href="<?php echo $basePath; ?>assets/css/global.css">
 </head>
 
 <body>
@@ -35,39 +33,34 @@
 
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
-                        <?php if (basename($_SERVER['SCRIPT_NAME']) !== 'index.php'): ?>
-                            <li class="nav-item">
-                                <a class="nav-link" href="../index.php">Accueil</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="country.php">Pays</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="culture.php">Culture</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="contact.php">Contact</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Mon Compte</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Déconnexion</a>
-                            </li>
-                        <?php endif; ?>
-                </ul>
-
-                <div>
-                    <?php if (basename($_SERVER['SCRIPT_NAME']) === 'index.php'): ?>
-                        <button class="btn btn-custom me-2 login-in">Connexion</button>
-                        <button class="btn btn-custom sign-in">Inscription</button>
+                    <?php if ($isLoggedIn): ?> <!-- Si l'utilisateur est connecté -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="../index.php">Accueil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../country.php">Pays</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../culture.php">Culture</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../contact.php">Contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Mon Compte</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../logout.php">Déconnexion</a>
+                        </li>
+                    <?php else: ?> <!-- Si l'utilisateur n'est pas connecté -->
+                        <li class="nav-item">
+                            <a class="btn btn-custom me-2 login-in" href="../views/login.php">Connexion</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="btn btn-custom sign-in" href="../views/register.php">Inscription</a>
+                        </li>
                     <?php endif; ?>
-                </div>
+                </ul>
             </div>
         </div>
     </nav>
-
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
